08e60657b85db82af5ec791e212a11c24e495925
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-04-11 14:27:32 +0500

    add user settings

--- Coworking.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Coworking.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -7,11 +7,18 @@ namespace Coworking.Infrastructure.Persistence;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : DbContext(options), IApplicationDbContext
 {
+    public DbSet<User> Users => Set<User>();
+    
     public DbSet<Room> Rooms => Set<Room>();
     public DbSet<Booking> Bookings => Set<Booking>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<User>().HasMany(u => u.Bookings)
+            .WithOne(b => b.User)
+            .HasForeignKey(b => b.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
         // Конфигурация цены с точностью до 2 знаков после запятой
         modelBuilder.Entity<Room>()
             .Property(r => r.PricePerHour)
