e4febda7589b582825f6cb34b9dcb313f9796882
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-04-16 16:29:13 +0500

    update CreationTime in EntityBase to use UTC for consistency

--- Coworking.Domain/Entity/EntityBase.cs
+++ Coworking.Domain/Entity/EntityBase.cs
@@ -3,6 +3,6 @@
 public class EntityBase
 {
     public Guid  Id { get; set; }
-    public DateTime CreationTime { get; set; } = DateTime.Now;
+    public DateTime CreationTime { get; set; } = DateTime.UtcNow;
     public DateTime? LastModificationTime { get; set; }
 }
