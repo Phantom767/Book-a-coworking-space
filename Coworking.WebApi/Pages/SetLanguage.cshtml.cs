79c1eceffd92ec69f8537e031ed8a0b93e7b3847
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-04-27 22:33:33 +0500

    add set language management page

---
+++ Coworking.WebApi/Pages/SetLanguage.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coworking.WebApi.Pages;
+
+public class SetLanguageModel : PageModel
+{
+    public IActionResult OnGet(string culture, string returnUrl = "/")
+    {
+        var supported = new[] { "ru", "kk", "en" };
+
+        if (!supported.Contains(culture))
+            culture = "ru";
+
+        Response.Cookies.Append(
+            CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+            new CookieOptions
+            {
+                Expires  = DateTimeOffset.UtcNow.AddYears(1),
+                IsEssential = true,
+                SameSite = SameSiteMode.Lax
+            }
+        );
+
+        return LocalRedirect(returnUrl);
+    }
+}
