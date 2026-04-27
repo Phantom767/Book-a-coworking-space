70a530537c387b624f3c4e2de15db35dd51e71bb
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-04-27 22:35:09 +0500

    add Login and Logout pages with localization support

---
+++ Coworking.WebApi/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Coworking.Application.DTOs.Auth;
+using Coworking.Application.Interfaces;
+using Coworking.Domain.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coworking.WebApi.Pages.Account
+{
+    public class LoginModel(
+        IAuthService authService,
+        SignInManager<ApplicationUser> signInManager,
+        UserManager<ApplicationUser> userManager)
+        : PageModel
+    {
+        [BindProperty] 
+        public LoginRequest Input { get; set; }
+
+        public void OnGet() { }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var result = await authService.LoginAsync(Input);
+
+            return await result.MatchAsync<IActionResult>(
+                async success =>
+                {
+                    var user = await userManager.FindByEmailAsync(Input.Email);
+                    if (user != null)
+                        await signInManager.SignInAsync(user, isPersistent: Input.RememberMe);
+
+                    var returnUrl = Request.Query["returnUrl"].FirstOrDefault();
+                    return LocalRedirect(returnUrl ?? "/Index");
+                },
+                errors =>
+                {
+                    ModelState.AddModelError(string.Empty, "Неверный логин или пароль");
+                    return Task.FromResult<IActionResult>(Page());
+                }
+            );
+        }
+    }
+}
---
+++ Coworking.WebApi/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Coworking.Domain.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coworking.WebApi.Pages.Account;
+
+public class LogoutModel(SignInManager<ApplicationUser> signInManager) : PageModel
+{
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await signInManager.SignOutAsync();
+            
+        return LocalRedirect("/");
+    }
+
+    public IActionResult OnGet()
+    {
+        return RedirectToPage("/Index");
+    }
+}
