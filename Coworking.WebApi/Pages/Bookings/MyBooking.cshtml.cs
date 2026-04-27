b56e129258f66f2afba592c4d189700e678bd9dd
Pulat Nurakhmet <nurahmetpulat@gmail.com>
2026-04-27 22:34:55 +0500

    add My Bookings page with booking details and localization support

---
+++ Coworking.WebApi/Pages/Bookings/MyBooking.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using Coworking.Application.DTOs;
+using Coworking.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coworking.WebApi.Pages.Bookings;
+
+public class MyBookingsModel(IBookingService bookingService) : PageModel
+{
+    public IEnumerable<BookingDto> Bookings { get; private set; } = new List<BookingDto>();
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (userIdClaim == null)
+        {
+            return RedirectToPage("/Account/Login"); // В Razor Pages лучше редиректить на логин
+        }
+
+        if (!Guid.TryParse(userIdClaim, out var userId))
+        {
+            return BadRequest("Invalid User ID");
+        }
+
+        var result = await bookingService.GetBookingsByUserAsync(userId);
+        // if (result.IsError)
+        // {
+        //     return StatusCode(500, "Ошибка при загрузке бронирований");
+        // }
+        Bookings = result;
+        return Page();    
+    }
+}
